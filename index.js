6313e02f4cd14a248e75153a86de5a084b319210
Lucas Werkmeister <mail@lucaswerkmeister.de>
2022-11-13 15:19:40 +0100

    Add initial functionality
    This is mostly the code I hacked together yesterday (see comment [1]),
    cleaned up (notably, removing callback_uri from the POST, which does
    nothing), with unit tests and a README added. Some of the names could
    probably still be improved upon.
    
    There are no integration tests yet; I think these will have to take the
    form of browser tests, so we can do the interactive authorization.
    
    [1]: https://github.com/lucaswerkmeister/m3api/issues/9#issuecomment-1312565935

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint camelcase: off */
+// OAuth 2.0 POST parameters use underscores
+
 import {
 	DEFAULT_OPTIONS,
 } from 'm3api/core.js';
@@ -8,10 +11,109 @@ import {
  *
  * @typedef Options
  * @type {Object}
+ * @property {OAuthClient} ['m3api-oauth2/client']
+ * The OAuth client, encapsulating the credentials.
+ * This option must be specified, there is no possible default value.
+ * (It’s recommended to set it in the session’s default options.)
  */
 
 Object.assign( DEFAULT_OPTIONS, {
+	'm3api-oauth2/client': { // the default is a fake OAuthClient to throw a more helpful exception
+		get consumerToken() {
+			throw new Error( 'The m3api-oauth2/client request option was not specified!' );
+		},
+	},
 } );
 
+/** @private */
+const secretTokenSymbol = Symbol( 'OAuthClient.secretToken' );
+
+/**
+ * An OAuth 2.0 client, encapsulating the public consumer token (or client ID)
+ * and the private secret token (or client secret).
+ */
+class OAuthClient {
+
+	/**
+	 * @param {string} consumerToken
+	 * @param {string} secretToken
+	 */
+	constructor( consumerToken, secretToken ) {
+		/** @public */
+		this.consumerToken = consumerToken;
+		/** @private */
+		Object.defineProperty( this, secretTokenSymbol, {
+			value: secretToken,
+		} );
+	}
+
+}
+
+/**
+ * Get the URL to authorize a user.
+ *
+ * You should send the user to this URL,
+ * then later call {@link handleCallback} once they’ve been redirected back to you.
+ *
+ * This function needs to be async for technical reasons,
+ * but should resolve basically immediately
+ * (no server communication is involved).
+ *
+ * @param {Session} session The session with which the authorization will be associated.
+ * @param {Options} [options] Request options.
+ * The 'm3api-oauth2/client' option must be specified
+ * either here or in the session’s default options.
+ * @return {string}
+ */
+async function getAuthorizeUrl( session, options = {} ) {
+	const { 'm3api-oauth2/client': client } = {
+		...DEFAULT_OPTIONS,
+		...session.defaultOptions,
+		...options,
+	};
+	const restUrl = session.apiUrl.replace( /api\.php$/, 'rest.php' );
+	const clientId = client.consumerToken;
+	return `${restUrl}/oauth2/authorize?response_type=code&client_id=${clientId}`;
+}
+
+/**
+ * Handle an authorization callback from the user.
+ *
+ * Call this method when the user returns from the {@link getAuthorizeUrl} result,
+ * with the full URL they were redirected to.
+ * The session will be set up for authenticated requests.
+ *
+ * @param {Session} session The session to which the authorization will apply.
+ * @param {string} callbackUrl The URL the user was redirected to.
+ * @param {Options} [options] Request options.
+ * The 'm3api-oauth2/client' option must be specified
+ * either here or in the session’s default options.
+ */
+async function handleCallback( session, callbackUrl, options = {} ) {
+	const { 'm3api-oauth2/client': client } = {
+		...DEFAULT_OPTIONS,
+		...session.defaultOptions,
+		...options,
+	};
+	const restUrl = session.apiUrl.replace( /api\.php$/, 'rest.php' );
+	const accessTokenUrl = `${restUrl}/oauth2/access_token`;
+	const code = new URL( callbackUrl ).searchParams.get( 'code' );
+	const { status, body } = await session.internalPost( accessTokenUrl, {}, {
+		grant_type: 'authorization_code',
+		code,
+		client_id: client.consumerToken,
+		client_secret: client[ secretTokenSymbol ],
+	}, { 'user-agent': session.getUserAgent( options ) } );
+
+	if ( status !== 200 ) {
+		throw new Error( `OAuth request returned non-200 HTTP status code: ${status}` );
+	}
+
+	session.defaultOptions.authorization = `Bearer ${body.access_token}`;
+}
+
 export {
+	OAuthClient,
+	getAuthorizeUrl,
+	handleCallback,
 };
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,13 +1,19 @@
 /* eslint-env mocha */
+/* eslint camelcase: off */
+// OAuth 2.0 POST parameters use underscores
 
 import { Session } from 'm3api/core.js';
 import {
+	OAuthClient,
+	getAuthorizeUrl,
+	handleCallback,
 } from '../../index.js';
+import { format } from 'util';
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 chai.use( chaiAsPromised );
 
-class _BaseTestSession extends Session {
+class BaseTestSession extends Session {
 
 	constructor( defaultParams = {}, defaultOptions = {} ) {
 		super( 'en.wikipedia.org', defaultParams, {
@@ -21,6 +27,135 @@ class _BaseTestSession extends Session {
 
 }
 
-it( 'will be removed once real functionality exists', async () => {
-	expect( 2 + 2 ).to.equal( 4 );
+const client = new OAuthClient( 'CLIENTID', 'CLIENTSECRET' );
+
+describe( 'OAuthClient', () => {
+
+	it( 'hides client secret', () => {
+		expect( String( client ) )
+			.not.to.contain( 'CLIENTSECRET' );
+		expect( format( client ) )
+			.not.to.contain( 'CLIENTSECRET' );
+		expect( JSON.stringify( Object.entries( client ) ) )
+			.not.to.contain( 'CLIENTSECRET' );
+	} );
+
+} );
+
+describe( 'getAuthorizeUrl', () => {
+
+	for ( const [ name, defaultOptions, options ] of [
+		[ 'defaultOptions', { 'm3api-oauth2/client': client }, {} ],
+		[ 'defaultOptions', {}, { 'm3api-oauth2/client': client } ],
+	] ) {
+		it( `gets client from ${name}`, async () => {
+			const session = new BaseTestSession( {}, defaultOptions );
+			expect( await getAuthorizeUrl( session, options ) )
+				.to.equal( 'https://en.wikipedia.org/w/rest.php/oauth2/authorize?response_type=code&client_id=CLIENTID' );
+		} );
+	}
+
+	it( 'throws if client option not specified', async () => {
+		await expect( getAuthorizeUrl( new BaseTestSession(), {} ) )
+			.to.be.rejectedWith( /m3api-oauth2\/client/ );
+	} );
+
+} );
+
+describe( 'handleCallback', () => {
+
+	for ( const [ name, defaultOptions, options ] of [
+		[ 'defaultOptions', { 'm3api-oauth2/client': client }, {} ],
+		[ 'defaultOptions', {}, { 'm3api-oauth2/client': client } ],
+	] ) {
+		it( `gets client from ${name} and gets access token`, async () => {
+			let called = false;
+			class TestSession extends BaseTestSession {
+				async internalPost( apiUrl, urlParams, bodyParams ) {
+					expect( apiUrl ).to.equal( 'https://en.wikipedia.org/w/rest.php/oauth2/access_token' );
+					expect( urlParams ).to.eql( {} );
+					expect( bodyParams ).to.eql( {
+						grant_type: 'authorization_code',
+						code: 'CODE',
+						client_id: 'CLIENTID',
+						client_secret: 'CLIENTSECRET',
+					} );
+					expect( called, 'not called yet' ).to.be.false;
+					called = true;
+					return {
+						status: 200,
+						headers: {},
+						body: {
+							token_type: 'Bearer',
+							expires_in: 14400,
+							access_token: 'ACCESSTOKEN',
+							refresh_token: 'REFRESHTOKEN',
+						},
+					};
+				}
+			}
+
+			const session = new TestSession( {}, defaultOptions );
+			await handleCallback( session, 'http://localhost:12345/oauth/callback?code=CODE', options );
+			expect( session.defaultOptions )
+				.to.have.property( 'authorization', 'Bearer ACCESSTOKEN' );
+			expect( called ).to.be.true;
+		} );
+	}
+
+	it( 'passes user agent into internalPost()', async () => {
+		let called = false;
+		class TestSession extends BaseTestSession {
+			async internalPost( apiUrl, urlParams, bodyParams, headers ) {
+				expect( headers )
+					.to.have.property( 'user-agent' )
+					.to.match( /^my-user-agent / );
+				expect( called, 'not called yet' ).to.be.false;
+				called = true;
+				return {
+					status: 200,
+					headers: {},
+					body: {
+						token_type: 'Bearer',
+						expires_in: 14400,
+						access_token: 'ACCESSTOKEN',
+						refresh_token: 'REFRESHTOKEN',
+					},
+				};
+			}
+		}
+
+		const session = new TestSession( {}, {
+			userAgent: 'my-user-agent',
+			'm3api-oauth2/client': client,
+		} );
+		await handleCallback( session, 'http://localhost:12345/oauth/callback?code=CODE' );
+		expect( called ).to.be.true;
+	} );
+
+	it( 'throws if status is not 200', async () => {
+		let called = false;
+		class TestSession extends BaseTestSession {
+			async internalPost() {
+				expect( called, 'not called yet' ).to.be.false;
+				called = true;
+				return {
+					status: 500,
+					headers: {},
+					body: {},
+				};
+			}
+		}
+
+		const session = new TestSession( {}, { 'm3api-oauth2/client': client } );
+		await expect( handleCallback( session, 'http://localhost:12345/oauth/callback?code=CODE' ) )
+			.to.be.rejectedWith( /500/ );
+		expect( called ).to.be.true;
+	} );
+
+	it( 'throws if client option not specified', async () => {
+		expect( handleCallback( new BaseTestSession(), '', {} ) )
+			.to.be.rejectedWith( /m3api-oauth2\/client/ );
+	} );
+
 } );
